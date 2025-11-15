ebe77caf51df03d4f5737a62eca849a82bbf7e7d
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-11-15 08:41:39 +0530

    added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/:date?" , (req , res) => {
   let dateString = req.params.date;
   try{
-    if(!dateString){
+    if(dateString === '' || dateString === undefined){
       date = new Date();
     
     }else{
